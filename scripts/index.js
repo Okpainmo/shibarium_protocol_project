42f664322c22ba1b20d294272e6272f4f8d5803e
Okpainmo <okpainmoandrew@gmail.com>
2023-01-30 15:37:43 +0100

    refactor: more dev progress

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,20 @@ const openBtn = document.querySelector('.mobile-menu-open-btn');
 const closeBtn = document.querySelector('.mobile-menu-close-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
 const formDOM = document.querySelector('.contact-form');
+const mobileMenuLink = document.querySelectorAll('.mobile-menu-link');
 
 // handling mobile menu toggle
 
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('open-mobile-menu');
   mobileMenu.classList.add('close-mobile-menu');
+});
+
+mobileMenuLink.forEach((each) => {
+  each.addEventListener('click', () => {
+    mobileMenu.classList.remove('open-mobile-menu');
+    mobileMenu.classList.add('close-mobile-menu');
+  });
 });
 
 openBtn.addEventListener('click', () => {
